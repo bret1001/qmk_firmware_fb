3d8d7798a7c32c80f4b71328425d2056831f6d7d
Francois Breton <bret1001@gmail.com>
2025-12-04 16:05:08 -0500

    Supprimer Print Screen et Insert de mNavSys (mac)

--- keyboards/zsa/voyager/keymaps/mykeymap/config.h
+++ keyboards/zsa/voyager/keymaps/mykeymap/config.h
@@ -9,7 +9,7 @@
 #define RGB_MATRIX_TIMEOUT 120000
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "waJ3V/yowRdr"
+#define SERIAL_NUMBER "waJ3V/pjzRYX"
 #define LAYER_STATE_8BIT
 #define COMBO_COUNT 1
 
